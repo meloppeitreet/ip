d7c8c6b5d0b0f681cda16b5f3dcdf5e401d06c2e
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2025-01-30 23:35:43 +0800

    Feature: Add input echo to EchoLex.java

--- src/main/java/EchoLex.java
+++ src/main/java/EchoLex.java
@@ -1,17 +1,44 @@
+import java.util.Scanner;
+
 public class EchoLex {
 
+    /** Chat Delimiter: Horizontal Line */
     static final String HORIZONTAL_LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
 
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println(" Hello! I'm EchoLex");
-        System.out.println(" What can I do for you?");
+        echoCommand(" Hello! I'm EchoLex" + "\n" + " What can I do for you?");
 
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println(" Bye. Hope to see you again soon!");
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
 
-        System.out.println(HORIZONTAL_LINE);
+        while (!userInput.equals("bye")) {
+            echoCommand(userInput);
+            userInput = in.nextLine();
+        }
+
+        echoCommand(" Bye. Hope to see you again soon!");
 
     }
+
+    /**
+     * Echoes commands entered by the user between horizontal lines.
+     *
+     * @param input Command entered by the user.
+     */
+    public static void echoCommand(String input) {
+
+        String indent = "    ";     // 4 spaces
+
+        System.out.println(indent + HORIZONTAL_LINE);
+        for (String line : input.split("\n")) {
+            System.out.println(indent + line);
+        }
+        System.out.println(indent + HORIZONTAL_LINE);
+
+        System.out.println();
+
+    }
+
+
 }
