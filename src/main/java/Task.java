ba9e969b156476dadf7bac329aaa628f51ea80ef
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2025-01-31 04:45:50 +0800

    Add task importance level enum to Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,19 @@
+enum Importance {
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
 public class Task {
 
     protected String description;
     protected boolean isDone;
+    private Importance importance;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        this.importance = Importance.LOW;
     }
 
     public void markDone() {
@@ -14,6 +22,32 @@ public class Task {
 
     public void unmarkDone() {
         isDone = false;
+    }
+
+    public void escalate() {
+        switch (importance) {
+        case LOW:
+            importance = Importance.MEDIUM;
+            break;
+        case MEDIUM:
+            importance = Importance.HIGH;
+            break;
+        default:
+            break;
+        }
+    }
+
+    public void deEscalate() {
+        switch (importance) {
+        case HIGH:
+            importance = Importance.MEDIUM;
+            break;
+        case MEDIUM:
+            importance = Importance.LOW;
+            break;
+        default:
+            break;
+        }
     }
 
     @Override
