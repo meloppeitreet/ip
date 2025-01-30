d034b30d580660c75be453d69fa373bed3b1db3b
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2025-01-31 00:17:54 +0800

    Feature: Add list memory command to EchoLex.java

--- src/main/java/EchoLex.java
+++ src/main/java/EchoLex.java
@@ -1,38 +1,64 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class EchoLex {
 
     /** Chat Delimiter: Horizontal Line */
     static final String HORIZONTAL_LINE = "____________________________________________________________";
+    /** User Input Text Memory */
+    private static ArrayList<String> memory = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        echoCommand(" Hello! I'm EchoLex" + "\n" + " What can I do for you?");
+        echoInput("Hello! I'm EchoLex" + "\n" + "What can I do for you?");
 
         Scanner in = new Scanner(System.in);
-        String userInput = in.nextLine();
+        String userInput = "";
+        boolean done = false;
 
-        while (!userInput.equals("bye")) {
-            echoCommand(userInput);
+        while (!done) {
             userInput = in.nextLine();
+            done = parseInput(userInput);
         }
-
-        echoCommand(" Bye. Hope to see you again soon!");
 
     }
 
     /**
-     * Echoes commands entered by the user between horizontal lines.
+     * Parses input entered by the user.
      *
-     * @param input Command entered by the user.
+     * @param input Input entered by the user.
      */
-    public static void echoCommand(String input) {
+    public static boolean parseInput(String input) {
+
+        switch (input) {
+        case "list":
+            echoInput(listCommand());
+            break;
+        case "bye":
+            echoInput("Bye. Hope to see you again soon!");
+            return true;
+        default:
+            memory.add(input);
+            echoInput("added: " + input);
+            break;
+        }
+
+        return false;
+
+    }
+
+    /**
+     * Echoes input between horizontal lines for chat formatting.
+     *
+     * @param input Input to be formatted between horizontal lines.
+     */
+    public static void echoInput(String input) {
 
         String indent = "    ";     // 4 spaces
 
         System.out.println(indent + HORIZONTAL_LINE);
         for (String line : input.split("\n")) {
-            System.out.println(indent + line);
+            System.out.println(indent + " " + line);
         }
         System.out.println(indent + HORIZONTAL_LINE);
 
@@ -40,5 +66,23 @@ public class EchoLex {
 
     }
 
+    /**
+     * Lists previous input from user saved in memory.
+     *
+     * @return Formatted memory string.
+     */
+    public static String listCommand() {
+
+        int counter = 1;
+        String result = "";
+
+        for (String input : memory) {
+            result = result.concat(counter + ". " + input + "\n");
+            counter++;
+        }
+
+        return result;
+
+    }
 
 }
