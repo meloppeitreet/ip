8e0dd01443d81931feb8180b8f7c245ccfd6de6e
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2025-01-30 22:48:38 +0800

    Override Duke.java with EchoLex.java

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# EchoLex User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/EchoLex.java
@@ -0,0 +1,17 @@
+public class EchoLex {
+
+    static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Hello! I'm EchoLex");
+        System.out.println(" What can I do for you?");
+
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Bye. Hope to see you again soon!");
+
+        System.out.println(HORIZONTAL_LINE);
+
+    }
+}
