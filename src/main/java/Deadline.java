496f2550bce987ad10a3cf5dfa299110eb13a58e
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2025-02-05 12:29:04 +0800

    Add date parsing functionality to EchoLex.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,20 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
+    private static DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+    private static final DateTimeFormatter SAVE_OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-    public Deadline(String description, Boolean isDone, String by) {
+    public Deadline(String description, Boolean isDone, LocalDateTime by) {
         super(description, isDone);
         this.by = by;
     }
 
     @Override
     public String saveFormat() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by.format(SAVE_OUTPUT_FORMATTER);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + by + ")";
+        return "[D]" + super.toString() + "(by: " + by.format(outputFormatter) + ")";
     }
 
 }
