554bd23838202a65c233844fe325c344704a6317
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2025-02-05 15:52:04 +0800

    Refactor EchoLex.java structure for more OOP

---
+++ src/main/java/Parser.java
@@ -0,0 +1,72 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+import java.time.temporal.ChronoField;
+
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class Parser {
+
+    /**
+     * Parses string into Command.
+     *
+     * @param commandString String input by user as command.
+     * @return Command object.
+     */
+    public static Command parse(String commandString) throws EchoLexException {
+
+        // Split Input into Main Command & Options
+        String[] parts = commandString.split("/");
+
+        // Main Command
+        String[] main = parts[0].split(" ", 2);
+        String command = main[0];
+        String argument = "";
+        if (main.length > 1) {
+            argument = main[1];
+        }
+
+        // Options
+        HashMap<String, String> options = new HashMap<>();
+        for (String option : Arrays.copyOfRange(parts, 1, parts.length)) {
+            String[] optionParts = option.split(" ", 2);
+            options.put(optionParts[0], optionParts[1]);
+        }
+
+        return new Command(command, argument, options);
+
+    }
+
+    /**
+     * Parse date in command string to LocalDateTime object.
+     *
+     * @param dateString Command string containing date.
+     * @return LocalDateTime object.
+     */
+    public static LocalDateTime parseDate(String dateString) throws EchoLexException {
+
+        try {
+
+            dateString = dateString.trim();
+            DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                    .appendOptional(DateTimeFormatter.ofPattern("yyyy-M-d"))
+                    .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                    .appendOptional(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+                    .appendOptional(DateTimeFormatter.ofPattern("MMM dd yyyy"))
+                    .parseDefaulting(ChronoField.ERA, 1)
+                    .toFormatter()
+                    .withResolverStyle(ResolverStyle.STRICT);
+
+            LocalDate date = LocalDate.parse(dateString, formatter);
+            return date.atStartOfDay();  // may be modified to accommodate time in the future
+
+        } catch (DateTimeParseException e) {
+            throw new EchoLexException("Invalid date: " + e.getMessage() + "\nPlease provide the date in yyyy-mm-dd format (e.g., 2019-10-15)");
+        }
+
+    }
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Ui {
+
+    /** Chat Indent */
+    static final String indent = "    ";     // 4 spaces
+    /** Chat Delimiter: Horizontal Line */
+    static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public Ui() { };
+
+    /**
+     * Shows welcome message to user.
+     */
+    public void showWelcome() {
+        boxInput("Hello! I'm EchoLex" + "\n" + "What can I do for you?");
+    }
+
+    /**
+     * Shows goodbye message to user.
+     */
+    public void showGoodbye() {
+        boxInput("Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Read command from user.
+     *
+     * @return Command String.
+     */
+    public String readCommand() {
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
+
+    /**
+     * Shows horizontal line.
+     */
+    public void showLine() {
+        System.out.println(indent + HORIZONTAL_LINE);
+    }
+
+    /**
+     * Echoes input between horizontal lines for chat formatting.
+     *
+     * @param input Input to be formatted between horizontal lines.
+     */
+    public void boxInput(String input) {
+
+        System.out.println(indent + HORIZONTAL_LINE);
+        for (String line : input.split("\n")) {
+            System.out.println(indent + " " + line);
+        }
+        System.out.println(indent + HORIZONTAL_LINE);
+        System.out.println();
+
+    }
+
+}
