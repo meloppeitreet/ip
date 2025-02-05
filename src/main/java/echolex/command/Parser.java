89e2f36d076040b1b937e90e166224aeec582323
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2025-02-05 16:29:07 +0800

    Organize src/main/java*.java into packages

--- src/main/java/echolex/command/Parser.java
+++ src/main/java/echolex/command/Parser.java
@@ -1,3 +1,7 @@
+package echolex.command;
+
+import echolex.error.EchoLexException;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
--- src/main/java/echolex/command/Ui.java
+++ src/main/java/echolex/command/Ui.java
@@ -1,3 +1,5 @@
+package echolex.command;
+
 import java.util.Scanner;
 
 public class Ui {
