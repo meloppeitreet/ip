efeb2eb839b6fbad18966f4917693f46a7e923b9
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2025-02-05 11:21:42 +0800

    Add save and load functionality to EchoLex.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -2,9 +2,14 @@ public class Deadline extends Task {
 
     protected String by;
 
-    public Deadline(String description, String by) {
-        super(description);
+    public Deadline(String description, Boolean isDone, String by) {
+        super(description, isDone);
         this.by = by;
+    }
+
+    @Override
+    public String saveFormat() {
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
     }
 
     @Override
